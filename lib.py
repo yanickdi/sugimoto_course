c3f31011d3921ad246b5a17f16a500000e81b1fe
mattposer <patrick.moser@edu.uni-graz.at>
2016-11-10 19:06:17 +0100

    random

--- lib.py
+++ lib.py
@@ -1,7 +1,7 @@
 import random
 
 def random_number_from_interval(lower, upper):
-    """Returns a random number out of the interval [lower, upper]"""
+    """Returns a random number out of the interval (lower, upper["""
     val = random.random()
     return lower + (upper -lower) * val
 
