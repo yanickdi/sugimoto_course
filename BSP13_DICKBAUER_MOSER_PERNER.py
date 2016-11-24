a48f48922ceca0716018fb90d28e1e24a5b102a3
Yanick Dickbauer <yanick@dickbauer.at>
2016-11-24 23:04:42 +0100

    BSP13 fertig

---
+++ BSP13_DICKBAUER_MOSER_PERNER.py
@@ -0,0 +1,33 @@
+"""
+    BSP09 - Simulation von PI
+    Dickbauer Yanick 1030489, Moser Patrick 1114954, Perner Manuel 0633155
+    WS 2016
+    
+    Comment: see Monte-Carlo-Simulation Wikipedia
+"""
+
+from lib import random_number_from_interval, euclidean_distance
+
+
+# INPUT:
+SIMULATIONS = 100000
+REFERENCE_PI = 3.14159265
+
+def main():
+    count = 0 # how often did we shoot in the unit circle
+    
+    for i in range(SIMULATIONS):
+        # create two randoms representing cords in a [1,1] rectangle
+        x = random_number_from_interval(0,1)
+        y = random_number_from_interval(0,1)
+        # check wheter these cords are lying within or beyond the unit circle
+        if euclidean_distance((0, 0), (x, y)) <= 1:
+            count += 1
+        
+    # avg_occurrences p should be pi/4 / 1 --> 4 * avg = pi
+    avg_occurrences = count/SIMULATIONS
+    pi = avg_occurrences * 4
+    print('Pi is simulated:', pi)
+    print('Difference:     {:+.5f} %'.format((pi/REFERENCE_PI - 1) * 100))
+   
+main()
