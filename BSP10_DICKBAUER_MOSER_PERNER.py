5b33fadb8a2e71e6763116b10d16ab15ebc514b0
manuelperner <manuelperner@gmail.com>
2016-11-24 17:50:24 +0100

    BSP10
    die Kindergarten-Elemente sind mal drinnen, jetzt wird’s knifflig…
    Das Feedback darüber, wie richtig/falsch der bet war muss programmiert
    werden.

--- BSP10_DICKBAUER_MOSER_PERNER.py
+++ BSP10_DICKBAUER_MOSER_PERNER.py
@@ -6,24 +6,45 @@
 
 from lib import random_number_from_interval
 
-#INPUT:
 
+#INPUT:
 NUMBER_OF_ROUNDS = 10
 NUMBER_OF_TIPS = 4
 NUMBERS = [1, 2, 3, 4, 5, 6]    #numbers used in this game
 NUMBER_OF_NUMBERS = 6   #TODO for code testing only 
 
-#def main():
-    #pass
+def main():
+    print()
+    #created_code = create_code()
+    created_code = [4,5,3,2]
+    #for i in range(NUMBER_OF_ROUNDS):
+    bet = ask_user_for_digits()
+    print(bet)
     
-
-def create_code(NUMBER_OF_NUMBERS):
+        #bet = [4, 5, 3, 2]
+        #print(created_code)
+        #if created_code == bet:
+         #   print('yay')
+        #else: 
+         #   print('fuck')
+    
+def create_code():
+    """This function creates a random 4-digit code with out of the numbers 1 to 6"""    
     code = []
-    for i in range(5):
-        code.append('1')
+    for i in range(4):
+        code.append(int(random_number_from_interval(1,6+1)))
     return code
-    print(code)
+    #print(code)
+
+def check_for_break():
+    print('Abbruchkriterien programmieren!')
+
+def ask_user_for_digits():
+    digits = []
+    for i in range(4):
+        digit = int(input('Please enter digit #{}: '.format(i+1)))
+        digits.append(digit)
+    return digits
 
 
-create_code(NUMBER_OF_NUMBERS)
-#main()
+main()
