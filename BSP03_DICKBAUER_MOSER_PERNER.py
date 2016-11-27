364f764e8d8ab725c099ce45e1cd7a049365bffa
yanick dickbauer <yanick@dickbauer.at>
2016-11-27 12:05:10 +0100

    bsp03 user input hinzugefügt

--- BSP03_DICKBAUER_MOSER_PERNER.py
+++ BSP03_DICKBAUER_MOSER_PERNER.py
@@ -3,23 +3,28 @@
     Dickbauer Yanick 1030489, Moser Patrick 1114954, Perner Manuel 0633155
     WS 2016
 """
-# INPUT:
-NUMBER_OF_POINTS = 10
+DEBUG = False
+
 #max, min:
 X_MAX = 20
 Y_MAX = 30
 
 
-from lib import random_number_from_interval
+from lib import random_number_from_interval, user_input
 
 def sort_key_function(input):
     # sort according to the points x value -> if its the same, according to the y value
     return (input[0], input[1])
 
 def main():
+    # user input:
+    inp = user_input((
+        ('Number of points', int, 10),), DEBUG)
+    number_of_points = inp[0]
+    
     # generate points:
     point_list = []
-    for i in range(NUMBER_OF_POINTS):
+    for i in range(number_of_points):
         point_x = random_number_from_interval(1, X_MAX)
         point_y = random_number_from_interval(1, Y_MAX)
         point = (point_x, point_y)
