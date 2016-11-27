a420e337098754d7c103d78d274433d233f755ff
yanick dickbauer <yanick@dickbauer.at>
2016-11-27 12:21:01 +0100

    bsp04 user input hinzugefügt

--- BSP04_DICKBAUER_MOSER_PERNER.py
+++ BSP04_DICKBAUER_MOSER_PERNER.py
@@ -3,12 +3,12 @@
     Dickbauer Yanick 1030489, Patrick Moser 1114954, Perner Manuel 0633155
     WS 2016
 """
-# INPUT:
-NUMBER_OF_SIMULATIONS = 100
-NUMBER_OF_PARTICIPANTS = 40
+DEBUG = False
+OPTION = False # enables optional feature: print all birthdays and
+               # mark those which occure more than twice
 DAYS_IN_A_YEAR = 365
 
-from lib import random_number_from_interval
+from lib import random_number_from_interval, user_input
 
 def occurrences(input_list):
     """Searches a list and counts for each element in the list the number of its occurence
@@ -27,43 +27,48 @@ def occurrences(input_list):
     return occ_dict
 
 def main():
+    #user input:
+    number_of_simulations, number_of_participants = user_input((
+        ('Number of simulations', int, 100),
+        ('Number of participants', int, 40)), DEBUG)
+        
     simulation_results = []
-    for simulation in range(NUMBER_OF_SIMULATIONS):
+    for simulation in range(number_of_simulations):
         # generate a list of birthdays - with length of participants
         birthday_list = []
-        for i in range(NUMBER_OF_PARTICIPANTS):
+        for i in range(number_of_participants):
             birthday_list.append( int(random_number_from_interval(0, DAYS_IN_A_YEAR))+1 )
         # count the same candidates:
         occ_dict = occurrences(birthday_list)
-        assert sum(occ_dict.values()) == NUMBER_OF_PARTICIPANTS
+        assert sum(occ_dict.values()) == number_of_participants
         number_of_same_values = sum([value if value >= 2 else 0 for key, value in occ_dict.items()])
         simulation_results.append(number_of_same_values)
-        for key, value in occ_dict.items():
-            if value >= 2:
-                print('Am Tag {} haben {} Personen Geburtstag'.format(key, value))
-        print()
-            
-        # output
-        print('Ergebnisse aus Simulation {}/{}:'.format(simulation+1, NUMBER_OF_SIMULATIONS))
-        for birthday in birthday_list:
-            # look if this one occures more than once
-            if occ_dict[birthday] >= 2:
-                print(birthday, '*')
-            else:
-                print(birthday)
-                
-        print('{} von {} Personen haben am gleichen Tag Geburtstag (p={:.2f}%).\n'.format(
-            number_of_same_values, NUMBER_OF_PARTICIPANTS, (number_of_same_values/NUMBER_OF_PARTICIPANTS)*100))
+        
+        # output only if option enabled:
+        if OPTION:
+            print('Ergebnisse aus Simulation {}/{}:'.format(simulation+1, number_of_simulations))
+            print('{} von {} Personen haben am gleichen Tag Geburtstag (p={:.2f}%).\n'.format(
+                number_of_same_values, number_of_participants, (number_of_same_values/number_of_participants)*100))    
+            for birthday in birthday_list:
+                # look if this one occures more than once
+                if occ_dict[birthday] >= 2:
+                    print(birthday, '*')
+                else:
+                    print(birthday)
+                    
+            for key, value in occ_dict.items():
+                if value >= 2:
+                    print('Am Tag {} haben {} Personen Geburtstag'.format(key, value))
+            print()
     
     # result of all simulations
     overall_number_of_same_birthdays = sum(simulation_results)
-    avg_number_of_same_birthdays = overall_number_of_same_birthdays / NUMBER_OF_SIMULATIONS
-    simulation_result_p = avg_number_of_same_birthdays / NUMBER_OF_PARTICIPANTS
+    avg_number_of_same_birthdays = overall_number_of_same_birthdays / number_of_simulations
+    simulation_result_p = avg_number_of_same_birthdays / number_of_participants
     
-    print()
-    print('Ergebnisse aller {0}/{0} Simulationen:'.format(NUMBER_OF_SIMULATIONS))
+    print('Ergebnisse aller {0}/{0} Simulationen:'.format(number_of_simulations))
     print('Durchschnittlich haben {} von {} Personen am gleichen Tag Geburtstag.'.format(
-        avg_number_of_same_birthdays, NUMBER_OF_PARTICIPANTS))
+        avg_number_of_same_birthdays, number_of_participants))
     print('WSKL ueber alle Simulationen: p={}%'.format(simulation_result_p * 100))
     
 main()
