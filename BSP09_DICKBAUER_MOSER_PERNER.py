06d9cb163e1586488b03049fc6822efc84c3184f
yanick dickbauer <yanick@dickbauer.at>
2016-11-27 15:42:15 +0100

    kleine änderungen in bsp09

--- BSP09_DICKBAUER_MOSER_PERNER.py
+++ BSP09_DICKBAUER_MOSER_PERNER.py
@@ -6,7 +6,6 @@
 from lib import random_number_from_interval, user_input
 
 DEBUG = True
-
 NUMBER_OF_SHOTS = 60
 
 def main():
@@ -30,11 +29,13 @@ def main():
         average_score_competition = sum(score_list_serie_competition)/NUMBER_OF_SHOTS
         avg_score_list_normal.append(average_score_normal)
         avg_score_list_competition.append(average_score_competition)
+        
+    # output
     print('The mean score per shot in normal is:',
-          round(mean_value(avg_score_list_normal), 2))
+          round(mean(avg_score_list_normal), 2))
     print('The variance of scores in normal is:', round(variance(avg_score_list_normal), 2))
     
-    print('The mean score per shot in competition is:', round(mean_value(avg_score_list_competition), 2))
+    print('The mean score per shot in competition is:', round(mean(avg_score_list_competition), 2))
     print('The variance of scores in competition is:', round(variance(avg_score_list_competition), 2))
     
 def random_shot():
@@ -78,14 +79,14 @@ def get_score_nerdy(x, y):
             return points
     return 0
 
-def mean_value(value_list):
+def mean(value_list):
     return sum(value_list)/len(value_list)
 
 def variance(value_list):
     summands = []
-    mean = mean_value(value_list)
+    mean_value = mean(value_list)
     for value in value_list:
-        summands.append((value - mean)**2)
-    return mean_value(summands)
+        summands.append((value - mean_value)**2)
+    return mean(summands)
 
 main()
