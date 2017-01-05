ee83472d1417837312c185353ea0e6ad52bb1f30
yanick dickbauer <yanick@dickbauer.at>
2017-01-05 17:54:47 +0100

    bsp20 fertig

--- BSP20_DICKBAUER_MOSER_PERNER.py
+++ BSP20_DICKBAUER_MOSER_PERNER.py
@@ -8,32 +8,81 @@ from lib import random_std
 
 SIMULATION = 55000
 NO_OF_VACUUM_TUBES = 4
-COST_SHUTDOWN = 100
-COST_TUBE = 20
+COST_SHUTDOWN_PER_HOUR = 100
+COST_PER_TUBE = 20
+HOURS_SHUTDOWN_ALL = 2
+HOURS_SHUTDOWN_ONE = 1
 
-def main ():
-    
+
+def simulate_replace_all():
+    print('Start simulation of replacing only one tube at once:')
     vacuum_tube_list = []
-    min_tube_lifetime = 0
     remain_hours = SIMULATION
-    remain_hours_list = []
+    count_replacement = 0
+    costs = 0
     
     while remain_hours >= 0:
+        vacuum_tube_list = []
+        # generate 4 new tubes:
         for i in range(NO_OF_VACUUM_TUBES):
-            vacuum_tube_lifetime = random_std(1500, 500)
+            vacuum_tube_lifetime = max(random_std(1500, 500), 0)
             vacuum_tube_list.append(vacuum_tube_lifetime)
-            min_tube_lifetime = min(vacuum_tube_list)
-  
-            remain_hours = remain_hours - min_tube_lifetime
-            remain_hours_list.append(remain_hours)
-        print(remain_hours_list)
-    
         
+        print('Remaing hours: {:.2f}h'.format(remain_hours))
+        print('Remaining lifetime of tubes before maintenance: {}'.format([round(elem, 2) for elem in vacuum_tube_list]))
+        min_tube_lifetime = min(vacuum_tube_list)
+        if min_tube_lifetime >= remain_hours:
+            print('No maintenance required till the end of simulation\n')
+            break
+        costs_of_period = COST_SHUTDOWN_PER_HOUR * HOURS_SHUTDOWN_ALL + NO_OF_VACUUM_TUBES * COST_PER_TUBE
+        costs += costs_of_period
+        remain_hours -= (min_tube_lifetime + HOURS_SHUTDOWN_ALL)
+        count_replacement += 1
+        print('Remaining lifetime of tubes after maintenance:  {}'.format([round(elem, 2) for elem in vacuum_tube_list]))
+        print('Costs of this period: {}'.format(costs_of_period))
+        print()
+    print('Costs of the policy of replacing all:', costs)
+    print('Nr. of shutdowns: {}'.format(count_replacement))
     
+def simulate_replace_one():
+    print('Start simulation of replacing only one tube at once:')
+    vacuum_tube_list = []
+    remain_hours = SIMULATION
+    count_replacement = 0
+    costs = 0
+    while remain_hours >= 0:
+        vacuum_tube_list = []
+        # generate 4 new tubes at start, and 1 in the following iteration:
+        for i in range(NO_OF_VACUUM_TUBES - len(vacuum_tube_list)):
+            vacuum_tube_lifetime = max(random_std(1500, 500), 0)
+            vacuum_tube_list.append(vacuum_tube_lifetime)
         
-
-
-
-
-
-main ()
+        print('Remaing hours: {:.2f}h'.format(remain_hours))
+        print('Remaining lifetime of tubes before maintenance: {}'.format([round(elem, 2) for elem in vacuum_tube_list]))
+        index_of_minimum = vacuum_tube_list.index(min(vacuum_tube_list))
+        min_tube_lifetime = vacuum_tube_list.pop(index_of_minimum)
+        if min_tube_lifetime >= remain_hours:
+            print('No maintenance required till the end of simulation\n')
+            break
+        
+        # reduce all lifetimes of all tubes:
+        for i in range(len(vacuum_tube_list)):
+            vacuum_tube_list[i] -= min_tube_lifetime
+        
+        costs_of_period = COST_SHUTDOWN_PER_HOUR * HOURS_SHUTDOWN_ONE + COST_PER_TUBE
+        costs += costs_of_period
+        remain_hours -= (min_tube_lifetime + HOURS_SHUTDOWN_ONE)
+        count_replacement += 1
+        print('Remaining lifetime of tubes after maintenance:  {}'.format([round(elem, 2) for elem in vacuum_tube_list]))
+        print('Costs of this period: {}'.format(costs_of_period))
+        print()
+    print('Costs of the policy of replacing only one:', costs)
+    print('Nr. of shutdowns: {}'.format(count_replacement))
+    
+    
+def main():
+    simulate_replace_all()
+    for i in range(5): print()
+    simulate_replace_one()
+    
+main()
