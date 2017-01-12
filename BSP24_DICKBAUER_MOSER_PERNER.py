612860e20ee25a61cd172edf9a3f194256f9fb14
manuelperner <manuelperner@gmail.com>
2017-01-12 12:12:58 +0100

    BSP24-27.py leer erstellt

---
+++ BSP24_DICKBAUER_MOSER_PERNER.py
@@ -0,0 +1,5 @@
+"""
+    BSP 24 - Waschstrasse
+    Dickbauer Yanick 1030489, Moser Patrick 1114954, Perner Manuel 0633155
+    WS 2016
+"""
---
+++ BSP27_DICKBAUER_MOSER_PERNER.py
@@ -0,0 +1,5 @@
+"""
+    BSP 27 - Epidemie
+    Dickbauer Yanick 1030489, Moser Patrick 1114954, Perner Manuel 0633155
+    WS 2016
+"""
