09ca51da9a634508fdd33280d725145ef6e71e88
yanick dickbauer <yanick@dickbauer.at>
2017-01-20 11:09:13 +0100

    bsp30 weitergemacht

--- BSP30_DICKBAUER_MOSER_PERNER.py
+++ BSP30_DICKBAUER_MOSER_PERNER.py
@@ -3,6 +3,8 @@
     Dickbauer Yanick 1030489, Moser Patrick 1114954, Perner Manuel 0633155
     WS 2016
 """
+from itertools import chain # this import from python's stdlibrary
+                            # makes our simulation a little bit faster, i hope its ok
 from lib import random_exp
 
 FREQUENCY = 1 # simulations per minute
@@ -50,6 +52,20 @@ def check_cash_desk(queues, rem_service_times):
                     next_customer_type = queue_before_desk.pop(0)
                     next_service_time = generate_service_time(next_customer_type)
                     rem_service_times[desk_type][desk_nr] = next_service_time
+
+def check_new_arrivals(next_arrivals, queues):
+    def _best_queue(type, queue):
+        if type = 'normal':
+            # this customer can only choose between normal queues
+            possible_queues = queues['normal']
+        elif type = 'express':
+            # this customer can choose between normal queues and of course - express queues
+            possible_queues = chain(queues['normal'], queues['express'])
+        # TODO: look up the shortest queue and return it
+    
+    for type, arrival in next_arrivals.items():
+        if arrival == 0:
+            # a new customer of type `type` arrived - add it to best queue
     
 def print_status(t, queues, rem_service_times):
     print('Iteration :{}'.format(t))
@@ -68,13 +84,17 @@ def simulate_cash_desks(nr_normal_desk, nr_expess_desk):
     types = [('normal', nr_normal_desk), ('express', nr_expess_desk)]
     queues  = {key: [[] for i in range(nr)] for key, nr in types}
     rem_service_times = {key: [None for i in range(nr)] for key, nr in types}
+    next_arrivals = {key: 0 for key, nr in types} # at the beginning of sim, two arrivals!
     
     for t in range(NR_SIM_STEPS):
+        # check new arrivals:
+        
         # check desks if one is idle or just finished a customer
         check_cash_desk(queues, rem_service_times)
         
         
         print_status(t, queues, rem_service_times)
+        
         # finished iteration, decrease all times:
         for rem_time_list in rem_service_times.values():
             for i, time in enumerate(rem_time_list):
